449d5536777543965bb2b8cd1e60da88b4102e9d
Khush Seervi <68182721+MistoMind@users.noreply.github.com>
2023-01-30 22:33:19 +0530

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -1,10 +1,13 @@
 from models import Base, User, session
 
 Base.metadata.create_all()
+
+# Currently User can be added using this file but in later commit's I'll be making user creation with Keycloak User Register.
+
 user = User()
-user.name = "Khush Seervi"
-user.email = "khush@gmail.com"
-user.password = "12345678"
-user.premium = True
+user.name = "Example Name"
+user.email = "example@mail-server.com"
+user.password = "example-password"
+user.premium = False
 session.add(user)
 session.commit()
